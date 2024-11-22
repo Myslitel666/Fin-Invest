eadeb2073d47ca8a0e6f8e52876a0605dda44fd5
Myslitel666 <tretyakov_02@bk.ru>
2024-11-22 20:56:26 +0500

    i

--- src/components/Box.svelte
+++ src/components/Box.svelte
@@ -2,7 +2,10 @@
     import type { IColorThemeStore } from "svelte-elegant/interfaces";
     import { themeStore } from "svelte-elegant/stores/themeStore";
 
-    export let src = '';
+    export let stockId: number;
+	export let stockTitle: string;
+	export let stockLogo: string;
+	export let price: string;
 
     let theme: IColorThemeStore | undefined;
 
@@ -16,14 +19,50 @@
     class = 'box'
     style:border = {`1px solid ${theme?.border.disabled.color}`}
     style:height = 5rem
-    style:width = 15rem
+    style:width = 35rem
     style:border-radius = {theme?.border.borderRadius}
     style:--bg-color = {theme?.disabled.touch}
 >
-    
+    <img src={stockLogo} alt={stockTitle} class="stock-logo" />
+    <div 
+        class = 'box-content'
+    >
+        <p>{stockTitle}</p>
+        <p 
+            class="price"
+            style:color={theme?.colors.primary}
+        >
+            Price: {price}
+        </p>
+    </div>
 </div>
 
 <style>
+    .box {
+		display: flex;
+		align-items: center;
+		padding: 1rem;
+		border: 1px solid #ddd;
+		border-radius: 0.5rem;
+
+		width: 200px;
+	}
+
+    .box-content {
+        margin-left: 1rem;
+    }
+
+	.stock-logo {
+		width: 80px;
+		height: 80px;
+		border-radius: 50%; /* Делаем изображение круглым */
+		margin-bottom: 0.5rem;
+	}
+
+	.price {
+		font-weight: bold;
+	}
+
     .box:hover {
         cursor:pointer;
         background-color: var(--bg-color);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,64 @@
 <script lang="ts">
 	import Box from './../components/Box.svelte';
+
+	// Массив структур stocks
+	let stocks = [
+		{
+			stockId: 1,
+			stockTitle: 'Apple',
+			stockLogo: 'https://via.placeholder.com/100',
+			price: '15,000 ₽'
+		},
+		{
+			stockId: 2,
+			stockTitle: 'Google',
+			stockLogo: 'https://via.placeholder.com/100',
+			price: '12,000 ₽'
+		},
+		{
+			stockId: 3,
+			stockTitle: 'Microsoft',
+			stockLogo: 'https://via.placeholder.com/100',
+			price: '20,000 ₽'
+		},
+		{
+			stockId: 2,
+			stockTitle: 'Google',
+			stockLogo: 'https://via.placeholder.com/100',
+			price: '12,000 ₽'
+		},
+		{
+			stockId: 3,
+			stockTitle: 'Microsoft',
+			stockLogo: 'https://via.placeholder.com/100',
+			price: '20,000 ₽'
+		},
+		{
+			stockId: 3,
+			stockTitle: 'Microsoft',
+			stockLogo: 'https://via.placeholder.com/100',
+			price: '20,000 ₽'
+		},
+	];
 </script>
 
 <section>
-	<Box/>
+	{#each stocks as stock}
+		<Box
+			stockId={stock.stockId}
+			stockTitle={stock.stockTitle}
+			stockLogo={stock.stockLogo}
+			price={stock.price}
+		/>
+	{/each}
 </section>
 
 <style>
 	section {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 		padding: 1rem;
+		gap: 1rem; /* Расстояние между карточками */
 	}
 </style>
