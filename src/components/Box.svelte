fa037bbbdb7d9acb51ad4f4e10dcbc24dcbc459c
Myslitel666 <tretyakov_02@bk.ru>
2024-11-22 19:59:13 +0500

    first commit

---
+++ src/components/Box.svelte
@@ -0,0 +1,31 @@
+<script lang='ts'>
+    import type { IColorThemeStore } from "svelte-elegant/interfaces";
+    import { themeStore } from "svelte-elegant/stores/themeStore";
+
+    export let src = '';
+
+    let theme: IColorThemeStore | undefined;
+
+    // Подписываемся на изменения темы
+    themeStore.subscribe(value => {
+        theme = value; //Инициализация объекта темы
+    });
+</script>
+
+<div 
+    class = 'box'
+    style:border = {`1px solid ${theme?.border.disabled.color}`}
+    style:height = 5rem
+    style:width = 15rem
+    style:border-radius = {theme?.border.borderRadius}
+    style:--bg-color = {theme?.disabled.touch}
+>
+    
+</div>
+
+<style>
+    .box:hover {
+        cursor:pointer;
+        background-color: var(--bg-color);
+    }
+</style>
